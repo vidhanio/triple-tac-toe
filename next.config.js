68aeb1ffb7d23a118100a45cb22bf84bcbc634e3
Vidhan Bhatt <me@vidhan.io>
2021-12-06 12:11:25 -0500

    hmm

--- next.config.js
+++ next.config.js
@@ -2,4 +2,16 @@
 module.exports = {
   reactStrictMode: true,
   basePath: "/triple-tac-toe",
+  async rewrites() {
+    return [
+      {
+        source: "/",
+        destination: "/triple-tac-toe",
+      },
+      {
+        source: "/:path*",
+        destination: "/triple-tac-toe/:path*",
+      },
+    ];
+  },
 };
